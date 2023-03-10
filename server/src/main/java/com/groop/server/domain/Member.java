619b670a8e3b4fffad4a703765c73154d9918af1
joandyalejo <joandyalejogarcia@outlook.com>
2023-03-09 22:39:49 -0500

    database setup with docker-compose.yml

---
+++ server/src/main/java/com/groop/server/domain/Member.java
@@ -0,0 +1,43 @@
+package com.groop.server.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author joandy alejo garcia
+ */
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
