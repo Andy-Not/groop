859a2ac021535483f193b0a943a30a7cbf7348f3
joand <joandyalejogarcia@outlook.com>
2023-03-14 14:49:31 -0400

    created a password encoder with currently a hardcoded password

--- server/src/main/java/com/groop/server/config/SecurityConfig.java
+++ server/src/main/java/com/groop/server/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.groop.server.config;
 
+import com.groop.server.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author joandy alejo garcia
@@ -16,10 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
    @Autowired
     private UserDetailsService userDetailsService;
 
+   @Autowired
+   private CustomPasswordEncoder customPasswordEncoder;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(customPasswordEncoder.getPasswordEncoder());
     }
 
 
