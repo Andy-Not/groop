348be7fca1154f1bf39c621009e5729727fcd7ce
joandyalejo <joandyalejogarcia@outlook.com>
2023-05-30 02:21:43 -0400

    Delete End point for task

--- server/src/main/java/com/groop/server/service/TaskService.java
+++ server/src/main/java/com/groop/server/service/TaskService.java
@@ -5,10 +5,16 @@ import com.groop.server.domain.Task;
 import com.groop.server.dto.TaskDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * @author joandy alejo garcia
  */
 public interface TaskService {
     Task saveNewTask(TaskDTO taskDTO);
+
+    void  deleteTask(Task task);
+
+    Optional<Task> findTask(Long id);
 
 }
--- server/src/main/java/com/groop/server/service/TaskServiceImpl.java
+++ server/src/main/java/com/groop/server/service/TaskServiceImpl.java
@@ -5,6 +5,8 @@ import com.groop.server.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 /**
  * @author joandy alejo garcia
  */
@@ -19,11 +21,20 @@ public class TaskServiceImpl implements  TaskService{
         return taskRepository.save(convertTaskDTOtoTask(taskDTO));
     }
 
+    @Override
+    public void deleteTask(Task task) {
+        taskRepository.delete(task);
+    }
+
     public Task convertTaskDTOtoTask(TaskDTO taskDTO){
         Task task = new Task();
         task.setTitle(taskDTO.getTitle());
         task.setDescription(taskDTO.getDescription());
         task.setStatus(taskDTO.getStatus());
         return task;
+    }
+
+    public Optional<Task> findTask(Long id){
+        return taskRepository.findById(id);
     }
 }
