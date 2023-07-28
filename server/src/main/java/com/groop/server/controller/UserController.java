8a4d3b4100c2b3ce2dbe39eb280cdfb2a263e8f6
joandyalejo <joandyalejogarcia@outlook.com>
2023-07-28 18:36:57 -0400

    new user endpoint with password and username validation

---
+++ server/src/main/java/com/groop/server/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.groop.server.controller;
+
+import com.groop.server.dto.SignUpDTO;
+import com.groop.server.dto.UserDTO;
+import com.groop.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author joandy alejo garcia
+ */
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/newUser")
+    public ResponseEntity<?> createNewUser(@RequestBody SignUpDTO signUp){
+        String username = signUp.getUsername();
+        String password = signUp.getPassword();
+
+        if (!userService.isUsernameValid(username))
+            return new ResponseEntity<>("username is taken", HttpStatus.BAD_REQUEST);
+
+        if (!userService.isPasswordValid(password))
+            return new ResponseEntity<>("weak password", HttpStatus.BAD_REQUEST);
+
+
+        userService.createNewUser(username, password);
+        return new ResponseEntity<>("new user has been created", HttpStatus.ACCEPTED);
+
+    }
+}
