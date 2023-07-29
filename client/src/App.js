f4f1b90d40dfa61ec0e1d34f03ca2e1594ab6ba7
joandyalejo <joandyalejogarcia@outlook.com>
2023-07-28 22:17:53 -0400

    built the signup fe UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,17 @@
 import Boards from "./pages/Boards";
 import SidebarWithHeader from "./component/nav/SidebarWithHeader";
-import { useContext, useEffect } from "react";
-import axios from "axios";
-import { GlobalKanbanStateContext } from "./store/KanbanContext";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PrivateRouteLogin from "./privateRoutes/PrivateRouteLogin";
+import SignUp from "./pages/SignUp";
 function App() {
-  const [globalState, setGlobalState] = useContext(GlobalKanbanStateContext);
-
-  // useEffect(() => {
-  //   console.log("use effect ran call was made");
-  //   const kanbanArr = [];
-  //
-  //   axios.get("api/kanban/getAllKanban").then((e) => {
-  //     e.data.forEach((e) => {
-  //       kanbanArr.push(e);
-  //     });
-  //     setGlobalState(kanbanArr);
-  //   });
-  // }, [setGlobalState]);
-  // console.log(globalState);
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<SignUp />} />
         <Route
           path={"/dashboard"}
           element={
